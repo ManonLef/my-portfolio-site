cc85afbeb6dea6a93e1a3c7c9308cb6cdfaf1662
Manon <manonlefkos@gmail.com>
2023-07-21 23:43:49 +0200

    add HTMLWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 entry: "./src/index.js",
+plugins: [
+    new HtmlWebpackPlugin(),
+  ],
 output: {
   filename: "main.js",
   path: path.resolve(__dirname, "dist"),
+  clean: true,
 },
 mode: 'development',
 module: {
